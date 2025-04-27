554c96bbd0608bc849c9d066df245e6ace2ead9d
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-26 22:18:14 -0700

    Add More Properties to Book Object For More Detail

--- javascript.js
+++ javascript.js
@@ -1,25 +1,27 @@
 const myLibrary = [];
 
-function Book(title, author, id) {
+function Book(title, author, pageNum, read, id) {
     if (!new.target) {
         throw Error("new not used to create Book Object.")
     }
     this.title = title;
     this.author = author;
     this.id = id;
+    this.pageNum = pageNum,
+    this.read = read
 }
 
-function addBookToLibrary(title, author) {
+function addBookToLibrary(title, author, pageNum, read) {
     let id = crypto.randomUUID();
-    let book = new Book(title, author, id)
+    let book = new Book(title, author, pageNum, read, id)
 
     myLibrary.push(book);
 }
 
-addBookToLibrary("Leviathan Wakes", "James S. A. Corey");
-addBookToLibrary("Childhood's End", "Arthur C. Clarke");
-addBookToLibrary("Mistborn: The Final Empire", "Brandon Sanderson");
-addBookToLibrary("Hyperion", "Dan Simmons");
+addBookToLibrary("Leviathan Wakes", "James S. A. Corey", 582, true);
+addBookToLibrary("Childhood's End", "Arthur C. Clarke", 224, true);
+addBookToLibrary("Mistborn: The Final Empire", "Brandon Sanderson", 655, true);
+addBookToLibrary("Hyperion", "Dan Simmons", 481, false);
 
 console.log(myLibrary);
 console.log(myLibrary[0].title);
@@ -30,7 +32,7 @@ function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
         let child = document.createElement("div");
         child.classList.add("card");
-        child.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}`;
+        child.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}\nPage Count: ${myLibrary[i].pageNum} Read: ${myLibrary[i].read}`;
 
         container.appendChild(child);
     }
@@ -50,8 +52,10 @@ addBookBtn.addEventListener("click", (e) => {
     
     let title = document.querySelector("#title");
     let author = document.querySelector("#author");
+    let pageNum = document.querySelector("#page-count");
+    let read = document.querySelector("#read-status");
 
-    addBookToLibrary(title.value, author.value);
+    addBookToLibrary(title.value, author.value, pageNum.value, read.checked);
     container.innerHTML = "";
     displayLibrary()
     modal.close();
